7992cd7198a57cff9e69cabb6d7104869afd9081
Nigora <nigoramamdv@gmail.com>
2023-10-11 10:42:05 +0500

    Running Router and Links

---
+++ src/NewPost.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const NewPost = ({handleSubmit, postBody, setPostBody, postTitle, setPostTitle}) => {
+  return (
+    <main className='NewPost'>
+     <h2>NewPost</h2>
+     <form className='newPostForm' onSubmit={handleSubmit}>
+      <label htmlFor="postTitle">Title</label>
+      <input
+       type="text" 
+       id='postTitle'
+       value={postTitle}
+       required
+       onChange={(e) => setPostTitle(e.target.value)}
+      />
+      <label htmlFor="postBody">Post</label>
+      <textarea
+        id='postBody'
+        required
+        value={postBody}
+        onChange={(e) => setPostBody(e.target.value)}
+      />
+      <button
+      type='submit'>Submit</button>
+
+     </form>
+    </main>
+  )
+}
+
+export default NewPost
